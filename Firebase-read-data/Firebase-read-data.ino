270ea9bb3476e4f031c48d3865c154ce260482ce
Changemin <bkm.change.min@gmail.com>
2019-12-12 01:09:52 +0900

    Complete : Data read

--- Firebase-read-data/Firebase-read-data.ino
+++ Firebase-read-data/Firebase-read-data.ino
@@ -1,13 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
-#define FIREBASE_HOST "YOUR FIREBASE HOST"
-#define FIREBASE_AUTH "YOUR FIREBASE KEY"
-#define WIFI_SSID "YOUR WIFI SSID"
-#define WIFI_PASSWORD "YOUR WIFI PASS"
+#define FIREBASE_HOST "test.firebaseio.com"
+#define FIREBASE_AUTH "YOUR KEY HERE"
+
+#define WIFI_SSID "YOUR SSID"
+#define WIFI_PASSWORD "YOUR PASS"
+
+const char* roomID = "0101";
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
@@ -17,22 +20,15 @@ void setup() {
   Serial.println();
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
-  
+
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.stream("");
+}
+
+int isDoor(){
+  return Firebase.getBool("/rooms/0101");
 }
 
 void loop() {
-  boolean Status = Firebase.getBool("isDoor");
-  Serial.print("READ : ");
-  Serial.println(Status);
-  delay(1000);
-
-  if(Status ==true){
-    // Door Open
-  }
-  else if(Status ==false){
-    // Door Close
-  }
-  // Firebase.remove("number"); //remove data
-  delay(1000);
+  Serial.println(isDoor());
 }
