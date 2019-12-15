530e6043512c10de4a2cd0f0daaa1358c393e835
Changemin <bkm.change.min@gmail.com>
2019-12-16 08:07:05 +0900

    Update : wifi-scan

--- wifi-scan/wifi-scan.ino
+++ wifi-scan/wifi-scan.ino
@@ -1,27 +1,41 @@
-
 #include "ESP8266WiFi.h"
 
-const char* ssid = "ssid"; //Enter SSID
-const char* password = "password"; //Enter Password
-
-void setup(void)
-{ 
+void setup() {
   Serial.begin(115200);
-  // Connect to WiFi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-     delay(500);
-     Serial.print("*");
-  }
-  
-  Serial.println("");
-  Serial.println("WiFi connection Successful");
-  Serial.print("The IP Address of ESP8266 Module is: ");
-  Serial.print(WiFi.localIP());// Print the IP address
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Setup done");
 }
 
-void loop() 
-{
-  // EMPTY
+void loop() {
+  Serial.println("scan start");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+    Serial.println("no networks found");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+
+  // Wait a bit before scanning again
+  delay(5000);
 }
